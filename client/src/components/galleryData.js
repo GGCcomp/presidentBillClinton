946f8781496a346ff5a743269ef0c47dc013e75c
GGCcomp <alok.akrt@gmail.com>
2025-11-24 14:32:07 +0530

    Update galleryData.js

--- client/src/components/galleryData.js
+++ client/src/components/galleryData.js
@@ -49,6 +49,14 @@ const galleryData = [
       "https://images.unsplash.com/photo-1494526585095-c41746248156?auto=format&fit=crop&w=800&q=80",
     link: "#",
   },
+  {
+    id: 7,
+    title: "Public Service ",
+    caption: "Encouraging youth at the annual youth conference.",
+    image:
+      "https://images.unsplash.com/photo-1494526585095-c41746248156?auto=format&fit=crop&w=800&q=80",
+    link: "#",
+  },
 ];
 
 export default galleryData;
